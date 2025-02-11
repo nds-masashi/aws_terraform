aac561e0f8c165c23bed8117425e907f7d7f3fe6
Nakamura Masashi <masashi@nds-tyo.co.jp>
2025-02-11 04:51:20 +0000

    redhatの構築

--- sample/modules/app/ec2.tf
+++ sample/modules/app/ec2.tf
@@ -5,10 +5,11 @@ resource "aws_security_group" "allow_ssh_ec2_sg" {
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [aws_vpc.vpc.cidr_block]
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    #cidr_blocks = [aws_vpc.vpc.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
     # EC2 Instance Coonect 経由で接続したい場合、0.0.0.0/0
     description = "ssh ssm"
   }
@@ -32,7 +33,7 @@ resource "aws_vpc_security_group_egress_rule" "https_rule" {
   from_port         = 443
   to_port           = 443
   ip_protocol       = "tcp"
-  cidr_ipv4         = aws_vpc.vpc.cidr_block
+  cidr_ipv4         = "0.0.0.0/0" # EC2でinstallしたい
   description       = "https ec2"
 }
 
@@ -68,8 +69,8 @@ resource "aws_iam_instance_profile" "ec2_instance_profile" {
 
 #EC2インスタンス
 resource "aws_instance" "ec2_instance_public" {
-  ami = "ami-0e2612a08262410c8" # AmazonLinux
-  #ami                    = "ami-0b512018294c0b386" # Redhat
+  #ami = "ami-0e2612a08262410c8" # AmazonLinux
+  ami                    = "ami-0b512018294c0b386" # Redhat
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.allow_ssh_ec2_sg.id]
@@ -80,14 +81,26 @@ resource "aws_instance" "ec2_instance_public" {
     http_tokens = "required"
   }
 
+  # Redhatの場合
+  user_data = <<-EOF
+    sudo dnf install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+    sudo systemctl enable amazon-ssm-agent
+    sudo systemctl start amazon-ssm-agent
+  EOF
+
+  # natgw 出来た後に installしないとエラーになる
+  depends_on = [
+    aws_nat_gateway.nat
+  ]
+
   tags = {
     Name = "${var.resourceName}-instance"
   }
 }
 
 resource "aws_instance" "ec2_instance_private" {
-  ami = "ami-0e2612a08262410c8" # AmazonLinux
-  #ami                    = "ami-0b512018294c0b386" # Redhat
+  #ami = "ami-0e2612a08262410c8" # AmazonLinux
+  ami                    = "ami-0b512018294c0b386" # Redhat
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.private_subnet.id
   vpc_security_group_ids = [aws_security_group.allow_ssh_ec2_sg.id]
@@ -97,6 +110,18 @@ resource "aws_instance" "ec2_instance_private" {
   metadata_options {
     http_tokens = "required"
   }
+
+  # Redhatの場合
+  user_data = <<-EOF
+    sudo dnf install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+    sudo systemctl enable amazon-ssm-agent
+    sudo systemctl start amazon-ssm-agent
+  EOF
+
+  # natgw 出来た後に installしないとエラーになる
+  depends_on = [
+    aws_nat_gateway.nat
+  ]
 
   tags = {
     Name = "${var.resourceName}-instance"
