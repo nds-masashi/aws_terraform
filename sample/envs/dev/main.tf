c55419fe38e5a12f7debbbc9bdaf3562c5df955f
Nakamura Masashi <masashi@nds-tyo.co.jp>
2025-03-02 04:41:01 +0000

    tag追加と変数追加

--- sample/envs/dev/main.tf
+++ sample/envs/dev/main.tf
@@ -1,3 +1,9 @@
 module "main" {
   source = "../../modules/app"
+
+  resourceName = "user"
+  email        = "user@sample.co.jp"
+  domain       = "mail.sample.co.jp"
+  user         = "xxxxx"
+  pass         = "xxxxx"
 }
